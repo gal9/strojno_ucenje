9079e010bab4e357ef1af448b268741f825dae80
Gal petkovšek <gp1914@student.uni-lj.si>
2021-12-21 13:27:33 +0100

    some results

--- notebooks/GP_regressor_hpyerparameter.py
+++ notebooks/GP_regressor_hpyerparameter.py
@@ -25,7 +25,7 @@ from src.Gen import Interval, Mnozica, Urejena_mnozica
 from src.Krizanje import Diagonalno_krizanje
 
 
-horizons = [3, 4, 5, 6, 7, 8, 9, 10]
+horizons = [4, 5, 9, 10]
 additional = [1, 2, 3, 4, 5, 6, 7, 10]
 features = [
     'level', 
